45b32478be112279577cb0fa684f6c32064b8591
Carson Reader <carsonthemonkey@gmail.com>
2023-10-25 02:27:00 -0700

    refactors buffer size

--- simfun.c
+++ simfun.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include "simply.h"
 
+#define MAX_INSTRUCTIONS 1024
+
 int main(int argc, char* argv[]){
 
     if (argc == 1)
@@ -11,21 +13,21 @@ int main(int argc, char* argv[]){
     }
 
     char *fileName = argv[1];
-    unsigned char buffer[256];
-
     
     FILE *file = fopen(fileName, "rb");
     if(file == NULL){
         fprintf(stderr, "Could not find specified input file\n");
         return 1;
     }
-    fread(buffer, sizeof(buffer), 1, file);
+
+    unsigned char buffer[MAX_INSTRUCTIONS];
+
+    fread(buffer, sizeof(unsigned char), MAX_INSTRUCTIONS, file);
     for(int i = 0; i < 256; i++){
         printf("%i\n", buffer[i]);
         if(buffer[i] == HLT){
             printf("HLT Detected: End File.\n");
             break;
         }
-        printf("%c", buffer[i]);
     }
 }
