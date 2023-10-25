ec5f641ea10fdb192e0d44899f2f87fb9eec4eb9
Kat Berge <kat.n.berge@gmail.com>
2023-10-25 12:41:04 -0700

    removes comment about bug that was fixed

--- square.s
+++ square.s
@@ -1,8 +1,5 @@
 # Lab 5: Square
-# written by Kat Berge, Carson Reader, Maximum DeCorso
-# There is an error with this as of right now according to Wyeth. Apparently LD can only load contents of registers into another register. 
-# This means we must initally load all constants into X0 using LDI and then transfer them to where we want them. 
-# The assembler will need to be updated to support this change as well.   
+# written by Kat Berge, Carson Reader, Maximum DeCorso  
     BRA start
 IN: 42
 OUT: 0
