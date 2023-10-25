0ca2bec9e54afcefdbeb743a54bf011f57940c07
Carson Reader <carsonthemonkey@gmail.com>
2023-10-25 01:40:33 -0700

    Refactors  simfun to use command line arguments

--- simfun.c
+++ simfun.c
@@ -1,24 +1,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void processBinaryFile(FILE *file){
+int main(int argc, char* argv[]){
 
-    unsigned char buffer[10];
-    FILE *file;
-    file = fopen("binary.bin", "rb");
-    fread(buffer, sizeof(buffer), 1, file);
+    if (argc == 1)
+    {
+        fprintf(stderr, "No target assembly file specified\n");
+        return 1;
+    }
+    char *fileName = argv[2];
 
-}
-
-int main(){
     unsigned char buffer[256];
-    FILE *file;
-    char fileName[10];
 
-    printf("Enter File Name: ");
-    scanf("%s", fileName);
-
-    file = fopen(fileName, "rb");
+    
+    FILE *file = fopen(fileName, "rb");
     fread(buffer, sizeof(buffer), 1, file);
     for(int i = 0; i < 256; i++){
         if(buffer[i] == 'H'){
