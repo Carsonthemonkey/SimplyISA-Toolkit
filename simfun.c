7eda4cbe4138afdd5fbab0944bba8201e3c3753a
Carson Reader <carsonthemonkey@gmail.com>
2023-10-25 09:44:59 -0700

    Adds function to decode operator

--- simfun.c
+++ simfun.c
@@ -4,6 +4,8 @@
 
 #define MAX_INSTRUCTIONS 1024
 
+int decode_operator(unsigned char instruction);
+
 int main(int argc, char* argv[]){
 
     if (argc == 1)
@@ -25,9 +27,29 @@ int main(int argc, char* argv[]){
     fread(program, sizeof(unsigned char), MAX_INSTRUCTIONS, file);
     for(int pc = 0; pc < 256; pc++){
         printf("%i\n", program[pc]);
+        int operator = decode_operator(program[pc]);
         if(program[pc] == HLT){
             printf("HLT Detected: End File.\n");
             break;
         }
     }
+}
+
+/**
+ * @brief Decodes the operator from the instruction passed to it by masking out the 5 least significant bits
+ * 
+ * @param instruction the instruction byte to decode
+ * @return int 
+ */
+int decode_operator(unsigned char instruction){
+    // 224 is a mask for 11100000
+    return instruction & 224;
+}
+
+/**
+ * @brief Displays the final program state
+ * 
+ */
+void display_program_state(){
+    //TODO: Implement
 }
